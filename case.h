a4744c2f8ef638a82005fe6b24ab177a64dfea41
cam38 <34076438+cam38@users.noreply.github.com>
2021-02-03 16:41:48 +0100

    Update case.h

--- case.h
+++ case.h
@@ -4,8 +4,20 @@
 
 class Case
 {
-public:
+protected :
+    Point position ;
+    
+public :
+    
     Case();
+    
+    void afficher() ;
+    void echanger(Case c1, Case c2) ;
+    Point getPosition() ;
+    void setPosition(Point p) ;
+    
+
+    
 };
 
 #endif // CASE_H
